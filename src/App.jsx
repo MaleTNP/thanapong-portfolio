41fe66b2f9e5dd656395dacad4954924b2366948
Thanapong Yooyen <malethanapong@gmail.com>
2025-09-11 00:38:31 +0700

    delete unnecessary file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
+import Header from './header/Header.jsx';
 
-function App() {
-  
-
+export default function App() {
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <Header></Header>
   )
 }
-
-export default App
